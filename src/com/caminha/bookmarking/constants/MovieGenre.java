a6392b1f80bc0e568b61b2a4d3515683e9fb135a
denis <dineresc@gmail.com>
2020-06-12 13:05:35 -0300

    add new constant movie gender

---
+++ src/com/caminha/bookmarking/constants/MovieGenre.java
@@ -0,0 +1,11 @@
+package com.caminha.bookmarking.constants;
+
+public class MovieGenre {
+
+    private MovieGenre() {}
+
+    public static final String CLASSSICS = "Classics";
+    public static final String HORROR = "Horror";
+    public static final String COMEDY = "Comedy";
+    public static final String DRAMA = "Drama";
+}
