06aa3b7f34356dfa92fc6fd65fadf19fae8fb7f5
denis <dineresc@gmail.com>
2020-06-12 13:04:23 -0300

    add new constnat

---
+++ src/com/caminha/bookmarking/constants/BookGenre.java
@@ -0,0 +1,13 @@
+package com.caminha.bookmarking.constants;
+
+public class BookGenre {
+
+    private BookGenre() {}
+
+    public static final String ART = "Art";
+    public static final String BIOGRAPHY = "Biography";
+    public static final String CHILDREN = "Children";
+    public static final String FICTION = "Fiction";
+    public static final String MISTERY = "Mistery";
+    public static final String ROMANCE = "Romance";
+}
---
+++ src/com/caminha/bookmarking/constants/Gender.java
@@ -0,0 +1,11 @@
+package com.caminha.bookmarking.constants;
+
+public class Gender {
+
+    private Gender() {}
+
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+    public static final int TRANS = 2;
+
+}
